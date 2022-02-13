004585a4bbbab36f86d56098df4ba90574df218b
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-02-13 19:16:19 +0330

    Add LinuxDirectoryProvider
    Uses XdgBasedirSpec and HomeBasedCommonPathProvider traits.

---
+++ src/PlatformSpecific/LinuxDirectoryProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MAChitgarha\Phirs\PlatformSpecific;
+
+use MAChitgarha\Phirs\Interfaces;
+use MAChitgarha\Phirs\Traits;
+
+class LinuxDirectoryProvider implements Interfaces\StandardDirectoryProvider
+{
+    use Traits\XdgBasedirSpec;
+    use Traits\HomeBasedCommonPathProvider;
+}
