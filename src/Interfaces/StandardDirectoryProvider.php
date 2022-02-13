cfbfa166c46fcdabcdf8ed5e24206d8719fde1b8
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-02-13 05:01:37 +0330

    Make all return type of methods in StandardDirectoryPro... nullable

--- src/Interfaces/StandardDirectoryProvider.php
+++ src/Interfaces/StandardDirectoryProvider.php
@@ -7,17 +7,17 @@ namespace MAChitgarha\Phirs\Interfaces;
  */
 interface StandardDirectoryProvider
 {
-    public function getHomePath(): string;
+    public function getHomePath(): ?string;
 
-    public function getCachePath(): string;
-    public function getConfigPath(): string;
-    public function getDataPath(): string;
+    public function getCachePath(): ?string;
+    public function getConfigPath(): ?string;
+    public function getDataPath(): ?string;
 
-    public function getDesktopPath(): string;
-    public function getDocumentPath(): string;
-    public function getDownloadPath(): string;
-    public function getMusicPath(): string;
-    public function getPicturePath(): string;
-    public function getPublicPath(): string;
-    public function getVideoPath(): string;
+    public function getDesktopPath(): ?string;
+    public function getDocumentPath(): ?string;
+    public function getDownloadPath(): ?string;
+    public function getMusicPath(): ?string;
+    public function getPicturePath(): ?string;
+    public function getPublicPath(): ?string;
+    public function getVideoPath(): ?string;
 }
