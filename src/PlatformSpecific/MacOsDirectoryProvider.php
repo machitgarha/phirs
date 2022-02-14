ba3c15215e39045a3cf96431daac165339f0747f
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-02-14 14:58:00 +0330

    Add MacOsDirectoryProvider, implementing StandardDirectoryProvider
    Add MoviesPathProvider also.

---
+++ src/PlatformSpecific/MacOsDirectoryProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MAChitgarha\Phirs\PlatformSpecific;
+
+use MAChitgarha\Phirs\Interfaces;
+use MAChitgarha\Phirs\Traits;
+
+class MacOsDirectoryProvider implements Interfaces\StandardDirectoryProvider
+{
+    use Traits\UnixLikeHomePathProvider;
+    use Traits\HomeBased\CommonPathProvider;
+    use Traits\HomeBased\DesktopPathProvider;
+    use Traits\HomeBased\MoviesPathProvider {
+        MoviesPathProvider::getVideosPath insteadof CommonPathProvider;
+    }
+    use Traits\HomeBased\PublicPathProvider;
+
+    private function getHomeLibraryChildPath(string ...$childPaths): string
+    {
+        return $this->getHomeChildPath('Library', ...$childPaths);
+    }
+
+    public function getCachePath(): string
+    {
+        return $this->getHomeLibraryChildPath('Caches');
+    }
+
+    private function getApplicationSupportPath(): string
+    {
+        return $this->getHomeLibraryChildPath('Application Support');
+    }
+
+    public function getConfigPath(): string
+    {
+        return $this->getApplicationSupportPath();
+    }
+
+    public function getDataPath(): string
+    {
+        return $this->getApplicationSupportPath();
+    }
+
+    public function getFontsPath(): string
+    {
+        return $this->getHomeLibraryChildPath('Fonts');
+    }
+
+    public function getPreferencesPath(): string
+    {
+        return $this->getHomeLibraryChildPath('Preferences');
+    }
+}
