43a46794706ad1fe0aff54a48f245f7ecee947fd
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-02-14 11:19:07 +0330

    Add LinuxDirectoryProvider::get{Fonts,Executables}Path()

--- src/PlatformSpecific/LinuxDirectoryProvider.php
+++ src/PlatformSpecific/LinuxDirectoryProvider.php
@@ -4,10 +4,21 @@ namespace MAChitgarha\Phirs\PlatformSpecific;
 
 use MAChitgarha\Phirs\Interfaces;
 use MAChitgarha\Phirs\Traits;
+use Symfony\Component\Filesystem\Path;
 
 class LinuxDirectoryProvider implements Interfaces\StandardDirectoryProvider
 {
     use Traits\XdgBasedirSpec;
     use Traits\HomeBasedCommonPathProvider;
     use Traits\TemplatesPathProvider;
+
+    public function getFontsPath(): string
+    {
+        return Path::join($this->getDataPath(), 'fonts');
+    }
+
+    public function getExecutablesPath(): string
+    {
+        return $this->getHomeChildPath('.local', 'bin');
+    }
 }
